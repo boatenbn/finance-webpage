6b93a7eab15f8d1235392827adc94f7bb8e36ccd
boatenbn <nsiahfia@gmail.com>
2025-03-19 01:07:48 -0400

    Update content.js

--- data/content.js
+++ data/content.js
@@ -1,1 +1,61 @@
+const data = {
+    header: {
+        title: "Balanced Prosperity with High Vibes and Low Costs",
+        subtitle: "The ultimate guide for achieving financial prosperity while keeping the vibes high and the costs low"
+    },
+    sections: [
+        {
+            title: "Welcome",
+            text: "intro text",
+            imgSrc: "images/Image1.webp",
+            
+        },
+        {
+            title: "What You'll Find",
+            imgSrc: "images/Budgeting-types-1140x1140.png",
+            text: "bullet point finds"
+        },
+        {
+            title: "Recent Posts",
+            posts: [
+                {
+                    imgSrc: "relatable image",
+                    title: "post title",
+                    text: "summary text"
+                }
+            ]
+        },
+        {
+            title: "Interesting Findings",
+            posts: [
+                {
+                    imgSrc: "relatable image",
+                    title: "post title",
+                    text: "summary text"
+                }
+            ]
+        },
+        {
+            title: "Resources", 
+            text: "bullet point resources",
+            imgSrc: "images/northfolk-Ok76F6yW2iA-unsplash.jpg"
+        },
+        {
+            title: "About",
+            imgSrc: "images/x037.jpg",
+            alt: "bevimage",
+            text: "about summary text"
+        }
+    ],
+    footer: {
+        title: "LET'S CONNECT AND BUILD COMMUNITY!",
+        text: "Let's vibe toward financial prosperity together!",
+        listItems: [
+            "Share your story",
+            "Join the community",
+            "Weekly updates"
+        ]
+    }
+};
 
+export default data;
